81b959670179a0319fb91543634d4f10b95eaf21
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-01-23 19:36:54 +0530

    test the module importing it in new file

---
+++ main.js
@@ -0,0 +1,24 @@
+import LinkedList from "./linkedList.js";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.size);
+console.log(list.head);
+console.log(list.tail);
+console.log(list.at(2));
+console.log(list.contains("dog"));
+console.log(list.find("snake"));
+console.log(list.toString());
+
+list.pop();
+list.insertAt("money", 1);
+list.removeAt(2);
+
+console.log(list.toString());
